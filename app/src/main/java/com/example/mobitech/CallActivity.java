8d17902fa5716bfc29990f69f0eaae767d121b50
Chintan22 <Dankharachintan@gmail.com>
2020-08-06 19:27:14 -0400

    Call Activity_Contact Us

---
+++ app/src/main/java/com/example/mobitech/CallActivity.java
@@ -0,0 +1,40 @@
+package com.example.mobitech;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+//import android.support.v4.app.ActivityCompat;
+//import android.support.v7.app.AppCompatActivity;
+
+public class CallActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
+        button = (Button) findViewById(R.id.buttonCall);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:1234567890"));
+
+                if (ActivityCompat.checkSelfPermission(CallActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
+
+    }
+}
