49c01710d247a0778c20309cec170889a54996a0
Puneeth996 <puneethreddy12@gmail.com>
2020-09-04 12:03:19 -0400

    model user 1

---
+++ app/src/main/java/com/example/mobitech/UserDo.java
@@ -0,0 +1,20 @@
+package com.example.mobitech;
+
+import java.io.Serializable;
+
+public class UserDo implements Serializable {
+
+    public String userId = "";
+    public String name = "";
+    public String email = "";
+    public String phone = "";
+    public String country = "";
+    public String city = "";
+    public String state = "";
+    public String zipCode = "";
+    public String gender = "";
+    public String password = "";
+
+    public UserDo(){}
+
+}
