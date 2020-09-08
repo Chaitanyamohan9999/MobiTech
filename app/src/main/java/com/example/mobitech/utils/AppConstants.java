5a0ef25d216302a885ccff266a849efd6ad8b81c
Chintan22 <dankharachintan@gmail.com>
2020-09-08 09:36:26 -0400

    utils_constants class added

---
+++ app/src/main/java/com/example/mobitech/utils/AppConstants.java
@@ -0,0 +1,9 @@
+package com.example.mobitech.utils;
+
+/**
+ * All application related constants are maintained by this class
+ */
+public class AppConstants {
+
+    public static final String userFile = "Users";
+}
